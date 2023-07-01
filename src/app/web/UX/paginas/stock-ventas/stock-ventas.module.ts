01615d3ff8d3d36339579a6b0b9d573322734baa
Diego Meza <diego-oz@hotmail.com>
2023-07-01 12:10:20 -0600

    Creación de componentes de stock de ventas

--- src/app/web/UX/paginas/stock-ventas/stock-ventas.module.ts
+++ src/app/web/UX/paginas/stock-ventas/stock-ventas.module.ts
@@ -5,9 +5,15 @@ import { StockVentaRoutingModule } from './stock-venta-routing.module';
 import { SharedModule } from 'src/app/web/informacion/utils/shared.module';
 import { ComponentesModule } from '../../componentes/componentes.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AgregarComponent } from './componentes/agregar/agregar.component';
+import { AgregarCategoriaComponent } from './componentes/agregar-categoria/agregar-categoria.component';
+import { CardProductoComponent } from './componentes/card-producto/card-producto.component';
+import { EditarComponent } from './componentes/card-producto/editar/editar.component';
+import { EliminarComponent } from './componentes/card-producto/eliminar/eliminar.component';
+import { InformacionComponent } from './componentes/card-producto/informacion/informacion.component';
 
 @NgModule({
-  declarations: [StockVentasComponent],
+  declarations: [StockVentasComponent, AgregarComponent, AgregarCategoriaComponent, CardProductoComponent, EditarComponent, EliminarComponent, InformacionComponent],
   imports: [
     CommonModule,
     StockVentaRoutingModule,
